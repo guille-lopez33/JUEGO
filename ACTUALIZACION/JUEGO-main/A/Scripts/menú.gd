cceb5263a738e44fbb953a26a408c05e716fc297
guille-lopez33 <4071035@alu.murciaeduca.es>
2025-01-15 13:29:54 +0100

    Add files via upload

---
+++ ACTUALIZACION/JUEGO-main/A/Scripts/menú.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+func _on_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_button_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_button_3_pressed():
+	get_tree().quit()
+	
+func _on_button_4_pressed():
+	
