9355b876fad94dcc8f046b00a68c9fa795ffd6e9
guille-lopez33 <4071035@alu.murciaeduca.es>
2025-01-21 14:29:52 +0100

    Add files via upload

---
+++ JUEGO-main/A/Scripts/menú.gd
@@ -0,0 +1,15 @@
+extends Control
+
+
+
+func _on_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_button_2_pressed():
+	pass # Replace with function body.
+
+
+func _on_button_3_pressed():
+	get_tree().quit()
+	
---
+++ JUEGO-main/ACTUALIZACION/JUEGO-main/A/Scripts/menú.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+func _on_opciones_pressed():
+	get_tree().change_scene_to_file("res://Escenas/OPCIONES.tscn")
+	
+	
