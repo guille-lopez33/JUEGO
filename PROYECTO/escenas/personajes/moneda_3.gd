5f542eb5d171c7410a68dedc3cab2395e25b85ee
guille-lopez33 <4071035@alu.murciaeduca.es>
2025-03-12 20:32:32 +0100

    Delete PROYECTO directory

--- PROYECTO/escenas/personajes/moneda_3.gd
+++
@@ -1,7 +0,0 @@
-extends Area2D
-
-
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("prota"):
-		queue_free()
-	get_tree().change_scene_to_file("res://escenas/fondos/control.tscn")
--- PROYECTO/scripts/menu/creditos.gd
+++
@@ -1,5 +0,0 @@
-extends Control
-
-
-func _on_button_4_pressed():
-	get_tree().change_scene_to_file("res://escenas/menu/menu.tscn")
